0dd3c713d1f7746cffb24e2cc78d236dd5c22d40
Documenter.jl <documenter@juliadocs.github.io>
2021-09-07 14:40:52 +0000

    build based on 8b3c6a0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MLBizOps","category":"page"},{"location":"#MLBizOps","page":"Home","title":"MLBizOps","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MLBizOps.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MLBizOps]","category":"page"}]
+}
