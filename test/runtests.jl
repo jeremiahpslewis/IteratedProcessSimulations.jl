3a9a3710d5969ba925760cf5d75f45e03af9e0d9
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2021-09-08 15:00:50 +0200

    Namespaces!

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using DataFrameMacros
 
 using Soss
 using MeasureTheory
+using StatsModels # TODO: remove from package dependencies
 using Chain
 
 import Distributions: Binomial
@@ -12,16 +13,32 @@ import GLM: glm, LogitLink, fit, coef, predict, @formula
 
 @testset "IteratedProcessSimulations.jl" begin
 	@testset "ml_biz_ops_sim" begin
+		test_dgp = @model params begin
+			income_eur ~ Normal(params[:mu_income], 0.1)
+			individual_attributes ~ Normal(1, 0.1)
+			default_prob = logistic(individual_attributes + income_eur)
+			default ~ Bernoulli(default_prob)
+		end
+		
+		n_epochs = 3
 
+		test_simulation_description = DataFrame(
+			"n_datapoints" => [200, 40, 30],
+			"mu_income" => fill(1, n_epochs),
+			"epoch" => 0:(n_epochs-1),
+		)
+
+		ips = IteratedProcessSimulation(test_dgp, test_simulation_description)#, generate_data, fit_model, summarize_model, choose_observations)
+		@test isa(ips, IteratedProcessSimulation)
 
 		function generate_data(ips::IteratedProcessSimulation, epoch::Int)
-			epoch_params = ips.simulation_params[epoch, :]
-			n_datapoints = epoch_params[!, :n_datapoints]
+			epoch_params = @chain ips.simulation_description @subset(:epoch == epoch) eachrow() _[1]
+			n_datapoints = epoch_params.n_datapoints
 		
 			# Generate one cycle of data
 			# TODO: Using 'burn in here', look into and drop this if unnecessary...
 			df = @chain epoch_params begin
-				ips.data_generating_process(_) # Apply epoch_params to the data_generating_process
+				ips.data_generating_process # Apply epoch_params to the data_generating_process
 				rand(n_datapoints * 5)
 				DataFrame()
 				last(n_datapoints)
@@ -29,11 +46,19 @@ import GLM: glm, LogitLink, fit, coef, predict, @formula
 		
 			# Add data generating process parameters for tracking
 			df = crossjoin(df, DataFrame(epoch_params)[!, [:epoch]])
-			df = @chain df @transform(:observed = true, :predicted_labels = nothing)
+			df = @chain df @transform(:observed = false, :predicted_labels = nothing)
 			
 			return df
 		end
-		
+
+		test_data = generate_data(ips, 0)
+		@test nrow(test_data) == 200
+		@test ncol(test_data) == 7
+
+		test_data_1 = generate_data(ips, 1)
+		@test nrow(test_data_1) == 40
+		@test ncol(test_data_1) == 7
+
 		function fit_model(ips::IteratedProcessSimulation, training_data::DataFrame, new_data::DataFrame, epoch::Int)
 			# Drop unobserved outcomes
 			training_data = @chain training_data @subset(:observed == true)
@@ -41,11 +66,17 @@ import GLM: glm, LogitLink, fit, coef, predict, @formula
 			# TODO: add example where model is dynamic, chosen via simulation parameters!
 			glm(@formula(default ~ 1 + income_eur), training_data, Binomial(), LogitLink())	
 		end
+
+		test_data = @chain test_data @transform(:observed = true)
+		m = fit_model(ips, test_data, test_data_1, 1)
+		@test coef(m) ≈ [0.18764107801174243, 0.06135920993917277]
 		
 		function summarize_model(ips::IteratedProcessSimulation, model::StatsModels.TableRegressionModel, simulation_data::DataFrame, new_data::DataFrame, epoch::Int)
 			DataFrame(:epoch => [epoch], :income_coef => coef(values(model))[1])
 		end
 		
+		@test summarize_model(ips, m, test_data, test_data, 1)[1, :income_coef] ≈ 0.019
+
 		function choose_observations(ips::IteratedProcessSimulation, model::StatsModels.TableRegressionModel, new_data::DataFrame, epoch::Int)
 			# Select 'unobserved' datapoints
 			new_data[!, :predicted_labels] = predict(model, new_data)
@@ -57,60 +88,14 @@ import GLM: glm, LogitLink, fit, coef, predict, @formula
 			return new_data
 		end		
 
-		test_dgp = @model params_1 begin
-			income_eur ~ Poisson(params_1[:mu_income])
-			individual_attributes ~ Normal(1, 10)
-			default_prob = logistic(individual_attributes + income_eur)
-			default ~ Bernoulli(default_prob)
-		end
-		
-		n_epochs = 3
-		n_data_points_per_epoch = 20
-
-		test_simulation_description = DataFrame(
-			"n_datapoints" => [20, 40, 30],
-			"mu_income" => 1:n_epochs,
-			"epoch" => 0:(n_epochs-1),
-		)
-
-		ips = IteratedProcessSimulation(test_dgp, test_simulation_description)
-		test_data = generate_data(ips, 0)
-		@test nrow(test_data) == 20
-		@test ncol(test_data) == 5
-
-		test_data = generate_data(ips, 1)
-		@test nrow(test_data) == 20
-		@test ncol(test_data) == 5
-
-		hist_params = @chain test_dgp_params @subset(:epoch == 0)
-		test_data = IteratedProcessSimulations.gather_historical_data(test_dgp, hist_params)
-		@test nrow(test_data) == 20
-		@test ncol(test_data) == 7
-
+		df = choose_observations(ips, m, test_data_1, 1)
+		@test nrow(df) == 40
+		@test (@chain df @combine(sum(:observed)) _[!, :observed_sum][1]) == 40
 	
-		function train_model_demo_1(training_data::DataFrame, epoch_params)
-			m = glm(epoch_params.model_formula, training_data, Binomial(), LogitLink())	
-			return m
-		end
 
-		function choose_and_label_data(unlabeled_data::DataFrame, model, epoch_params)
-			# feature_vars = epoch_params.feature_variables
-		
-			unlabeled_data[!, :predicted_labels] = predict(model, unlabeled_data)
-			
-			unlabeled_data = @chain unlabeled_data begin
-				# @transform(:observed = (rand() < 0.1))
-				@transform(:observed = true)
-				# @transform(:observed = (:predicted_labels < 0.10) | (rand() < 0.1))		
-			end
-			
-			
-			
-			return unlabeled_data
-		end
-
-		@test_throws MethodError ml_biz_ops_sim(DataFrame(), DataFrame(), train_model_demo_1, summarize_logit_model_test, choose_and_label_data)
-		@test_throws ArgumentError ml_biz_ops_sim(test_dgp, DataFrame(), train_model_demo_1, summarize_logit_model_test, choose_and_label_data)
+		# @test_throws 
+		run_simulation(ips)
+		# @test_throws ArgumentError run_(test_dgp, DataFrame(), train_model_demo_1, summarize_logit_model_test, choose_and_label_data)
 	
 		test_data, model_objs = ml_biz_ops_sim(test_dgp, test_dgp_params, train_model_demo_1, summarize_logit_model_test, choose_and_label_data)
 		@test ncol(test_data) == 8
