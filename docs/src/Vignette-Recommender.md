cfd2f4e23ab853de59c1dab95cf85fd510fbd59d
Jeremiah Lewis <--get>
2021-10-20 23:05:48 +0200

    Flesh out vignette

---
+++ docs/src/Vignette-Recommender.md
@@ -0,0 +1,182 @@
+# Recommender Vignette
+
+```julia
+using IteratedProcessSimulations
+using Recommendation
+using DataFrames
+using Soss
+using MeasureTheory
+using Chain
+using DataFrameMacros
+using UUIDs
+```
+
+## Simulation Premises
+
+A bookstore has customer account data on previous purchases
+as well as a monthly newsletter in which it can suggest three books to read,
+personalized to each customer.
+
+All books cost the same. Each book has latent attributes, quality and topicality,
+which are fixed. Each customer has unique preferences weighting these two factors
+and resulting in a utility score. The customer chooses the highest utility book each month,
+as long as it has a utility greater than 0
+(the utility of the most attractive non-book good available and average score across all books and attributes).
+
+New books are released each month. The bookstore uses a collaborative filter to
+identify optimal books to offer to each user. Once a user has chosen a book,
+its user-specific utility becomes visible to the bookstore (i.e. a rating).
+
+To gather feedback on newly released books, the bookstore distributes
+copies to 10% of users in the release month, in exchange for their rating
+which becomes available instantly.
+
+The simulation runs over a course of 36 months.
+
+## Simulate User Preferences
+
+```julia
+n_users = 500
+n_books_per_month = 10
+n_months = 36
+pct_pre_read = 0.1  # X% of new books are 'pre-read' by users
+
+# Define data generating process for the users
+user_dgp = @model params begin
+        utility_weight_quality_raw ~ Normal(0, 0.3)
+        user_utility_weight_quality = logistic(utility_weight_quality_raw)
+        user_utility_weight_topicality = 1 - user_utility_weight_quality
+end
+
+user_sim_description = DataFrame(
+    "n_datapoints" => [n_users],
+    "epoch" => [0],
+)
+```
+
+## Create user sample
+```julia
+
+user_attributes = @chain generate_data(user_dgp, user_sim_description) begin
+    @transform(:user_id = @c 1:length(:id))
+    @select(:user_id, :user_utility_weight_quality, :user_utility_weight_topicality)
+end
+```
+
+## Define data generating process for the books
+
+```julia
+book_dgp = @model params begin
+        quality ~ Normal(0, 5)
+        topicality ~ Normal(0, 5)
+end
+
+book_sim_description = DataFrame(
+    "n_datapoints" => fill(n_books_per_month, 36),
+    "epoch" => 1:36,
+)
+
+# TODO: remove line, just for testing
+book_attributes = generate_data(book_dgp, eachrow(book_sim_description)[2])
+```
+
+## Build user-book dataframe via `transform_data` function
+
+```julia
+function transform_data(book_df)
+
+    book_df = @chain book_df @transform(:book_id = @c 1:nrow(book_df)) @transform(:book_id = :book_id + (:epoch - 1) * nrow(book_df))
+    user_book_df = @chain book_df begin
+        crossjoin(user_attributes)
+    end
+
+    user_book_df = @chain user_book_df begin
+        @transform(
+            :user_book_utility = :topicality * :user_utility_weight_quality + :quality * :user_utility_weight_topicality,
+            # X% of new books are 'pre-read' by users
+            :observed = rand(Bernoulli(pct_pre_read))
+                   )
+    end
+
+    return user_book_df
+end
+
+# TODO: remove line, just for testing
+new_data = transform_data(book_attributes)
+```
+
+## Define Machine Learning Model
+
+```julia
+# TODO: remove line, just for testing
+training_data = new_data
+
+function convert_dataframe_to_recommender(df::DataFrame, n_users, n_books)
+    event_list = []
+    for row in eachrow(df)
+        # Here we assume that a user knows and reports their utility after having read the book
+        push!(event_list, Event(row[:user_id], row[:book_id], row[:user_book_utility]))
+    end
+
+    event_list = convert(Vector{Event}, event_list)
+    
+    data = DataAccessor(event_list, n_users, n_books)
+
+    return data
+end
+
+function fit_model(epoch_parameters::DataFrameRow, training_data::DataFrame, new_data::DataFrame)
+        append!(training_data, new_data, promote=true)
+
+        n_users = maximum(training_data[!, :user_id])
+        n_books =maximum(training_data[!, :book_id])
+        # Drop unobserved outcomes
+        training_data = @chain training_data @subset(:observed == true)
+            
+
+        data = convert_dataframe_to_recommender(training_data, n_users, n_books)
+        recommender = SVD(data, 10)
+        build!(recommender)
+
+        return recommender
+end
+
+fit_model(eachrow(book_sim_description)[1], training_data, new_data)
+```
+
+```julia
+# Skip using this for now, but could be useful in a real study...
+function summarize_model(epoch_parameters::DataFrameRow, model, simulation_data::DataFrame, new_data::DataFrame)
+    DataFrame(:epoch => [epoch_parameters.epoch])
+end
+```
+
+
+```julia
+function choose_observations(epoch_parameters::DataFrameRow, recommender, new_data::DataFrame, simulation_data::DataFrame)
+    # Select 'unobserved' datapoints to observe
+    append!(simulation_data, new_data, promote=true)
+
+    # Each user gets to read an additional book!
+    for user_id in unique((@chain simulation_data @subset(!:observed) _[!, :user_id]))
+        user_prediction = recommend(recommender, user_id, 1, (@chain new_data @subset(!:observed & (:user_id == user_id)) @select(:book_id) unique _[!, :book_id]))
+        best_book = user_prediction[1][1]
+        best_book_score = user_prediction[1][2]
+        # if best_book_score > 0 # If utility is positive, then flip to 'observed'
+        #     new_data[(new_data[!, :user_id] .== user_id) .& (new_data[!, :book_id] .== best_book), :observed] = true
+        # end
+    end
+
+    return simulation_data
+end	
+```
+
+
+## Put it all together and run the simulation
+
+
+```julia
+ips = IteratedProcessSimulation(book_dgp, book_sim_description, transform_data, fit_model, summarize_model, choose_observations)
+
+simulation_data, model_summary, model_objects = run_simulation(ips)
+```
--- test/runtests.jl
+++ test/runtests.jl
@@ -12,94 +12,94 @@ import Distributions: Binomial
 import GLM: glm, LogitLink, fit, coef, predict, @formula
 
 @testset "IteratedProcessSimulations.jl" begin
-	test_dgp = @model params begin
-		income_eur ~ Normal(params[:mu_income], 0.1)
-		individual_attributes ~ Normal(1, 0.1)
-		default_prob = logistic(individual_attributes + income_eur)
-		default ~ Bernoulli(default_prob)
-	end
-	
-	n_epochs = 3
+    test_dgp = @model params begin
+        income_eur ~ Normal(params[:mu_income], 0.1)
+        individual_attributes ~ Normal(1, 0.1)
+        default_prob = logistic(individual_attributes + income_eur)
+        default ~ Bernoulli(default_prob)
+    end
+    
+    n_epochs = 3
 
-	test_simulation_description = DataFrame(
-		"n_datapoints" => [200, 40, 30],
-		"mu_income" => fill(1, n_epochs),
-		"epoch" => 0:(n_epochs-1),
-	)
+    test_simulation_description = DataFrame(
+        "n_datapoints" => [200, 40, 30],
+        "mu_income" => fill(1, n_epochs),
+        "epoch" => 0:(n_epochs-1),
+    )
 
-	epoch_parameters = eachrow(test_simulation_description)[1]
+    epoch_parameters = eachrow(test_simulation_description)[1]
 
-	test_data = generate_data(test_dgp, epoch_parameters)
-	@test nrow(test_data) == 200
-	@test ncol(test_data) == 8
+    test_data = generate_data(test_dgp, epoch_parameters)
+    @test nrow(test_data) == 200
+    @test ncol(test_data) == 8
 
-	epoch_parameters = eachrow(test_simulation_description)[2]
-	test_data_1 = generate_data(test_dgp, epoch_parameters)
-	@test nrow(test_data_1) == 40
-	@test ncol(test_data_1) == 8
+    epoch_parameters = eachrow(test_simulation_description)[2]
+    test_data_1 = generate_data(test_dgp, epoch_parameters)
+    @test nrow(test_data_1) == 40
+    @test ncol(test_data_1) == 8
 
-	test_data_2 = generate_data(test_dgp, test_simulation_description)
-	@test nrow(test_data_2) == 270
-	@test ncol(test_data_2) == 8
+    test_data_2 = generate_data(test_dgp, test_simulation_description)
+    @test nrow(test_data_2) == 270
+    @test ncol(test_data_2) == 8
 
-	function fit_model(epoch_parameters::DataFrameRow, training_data::DataFrame, new_data::DataFrame)
-		# Drop unobserved outcomes
-		training_data = @chain training_data @subset(:observed == true)
-	
-		# TODO: add example where model is dynamic, chosen via simulation parameters!
-		glm(@formula(default ~ 1 + income_eur), training_data, Binomial(), LogitLink())	
-	end
+    function fit_model(epoch_parameters::DataFrameRow, training_data::DataFrame, new_data::DataFrame)
+        # Drop unobserved outcomes
+        training_data = @chain training_data @subset(:observed == true)
+    
+        # TODO: add example where model is dynamic, chosen via simulation parameters!
+        glm(@formula(default ~ 1 + income_eur), training_data, Binomial(), LogitLink())	
+    end
 
-	test_data = @chain test_data @transform(:observed = true)
-	m = fit_model(epoch_parameters, test_data, test_data_1)
-	# @test coef(m) ≈ [0.18764107801174243, 0.06135920993917277]
+    test_data = @chain test_data @transform(:observed = true)
+    m = fit_model(epoch_parameters, test_data, test_data_1)
+    # @test coef(m) ≈ [0.18764107801174243, 0.06135920993917277]
 
-	function transform_data(df::DataFrame)
-		return df
-	end
+    function transform_data(df::DataFrame)
+        return df
+    end
 
-	function summarize_model(epoch_parameters::DataFrameRow, model::StatsModels.TableRegressionModel, simulation_data::DataFrame, new_data::DataFrame)
-		DataFrame(:epoch => [epoch_parameters.epoch], :income_coef => coef(values(model))[1])
-	end
-	
-	# @test summarize_model(epoch_parameters, m, test_data, test_data)[1, :income_coef] ≈ 0.019
+    function summarize_model(epoch_parameters::DataFrameRow, model::StatsModels.TableRegressionModel, simulation_data::DataFrame, new_data::DataFrame)
+        DataFrame(:epoch => [epoch_parameters.epoch], :income_coef => coef(values(model))[1])
+    end
+    
+    # @test summarize_model(epoch_parameters, m, test_data, test_data)[1, :income_coef] ≈ 0.019
 
-	function choose_observations(epoch_parameters::DataFrameRow, model::Union{StatsModels.TableRegressionModel, Nothing}, new_data::DataFrame, simulation_data::DataFrame)
-		# Select 'unobserved' datapoints
+    function choose_observations(epoch_parameters::DataFrameRow, model::Union{StatsModels.TableRegressionModel, Nothing}, new_data::DataFrame, simulation_data::DataFrame)
+        # Select 'unobserved' datapoints
 
-		# All 'historical' data points are considered observed
-		if epoch_parameters.epoch < 1
-			new_data = @chain new_data begin
-				@transform(:observed = true)
-			end			
-		else
-			new_data[!, :predicted_labels] = predict(model, new_data)
-				
-			new_data = @chain new_data begin
-				@transform(:observed = true)
-			end	
-		end
+        # All 'historical' data points are considered observed
+        if epoch_parameters.epoch < 1
+            new_data = @chain new_data begin
+                @transform(:observed = true)
+            end			
+        else
+            new_data[!, :predicted_labels] = predict(model, new_data)
+                
+            new_data = @chain new_data begin
+                @transform(:observed = true)
+            end	
+        end
 
-		# Add new data to dataset
-		append!(simulation_data, new_data, promote=true)	
+        # Add new data to dataset
+        append!(simulation_data, new_data, promote=true)	
 
-		return simulation_data
-	end		
+        return simulation_data
+    end		
 
-	df = choose_observations(epoch_parameters, m, test_data_1, test_data_1)
-	@test nrow(df) == 80
-	@test (@chain df @combine(sum(:observed)) _[!, :observed_sum][1]) == 40
+    df = choose_observations(epoch_parameters, m, test_data_1, test_data_1)
+    @test nrow(df) == 80
+    @test (@chain df @combine(sum(:observed)) _[!, :observed_sum][1]) == 40
 
-	ips = IteratedProcessSimulation(test_dgp, test_simulation_description, transform_data, fit_model, summarize_model, choose_observations)
-	@test isa(ips, IteratedProcessSimulation)		
+    ips = IteratedProcessSimulation(test_dgp, test_simulation_description, transform_data, fit_model, summarize_model, choose_observations)
+    @test isa(ips, IteratedProcessSimulation)		
 
-	sim_data, model_summary, model_objects = run_simulation(ips)
+    sim_data, model_summary, model_objects = run_simulation(ips)
 
-	@test ncol(sim_data) == 9
-	@test nrow(sim_data) == 270
+    @test ncol(sim_data) == 9
+    @test nrow(sim_data) == 270
 
-	sim_data, model_summary, model_objects = run_simulation(ips, 5)
+    sim_data, model_summary, model_objects = run_simulation(ips, 5)
 
-	@test ncol(sim_data) == 9
-	@test nrow(sim_data) == 1350
+    @test ncol(sim_data) == 9
+    @test nrow(sim_data) == 1350
 end
