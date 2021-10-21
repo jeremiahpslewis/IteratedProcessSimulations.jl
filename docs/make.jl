68819b07b3a9c4aa31560af4eee2cb9730dbeccb
Jeremiah Lewis <--get>
2021-10-21 15:37:50 +0200

    Add vegalite obj show

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,16 @@
 using IteratedProcessSimulations
 using Documenter
+using VegaLite, UUIDs
+
+# Snippet source: https://github.com/queryverse/VegaLite.jl/blob/master/docs/make.jl
+function Base.show(io::IO, m::MIME"text/html", v::VegaLite.VLSpec)
+    divid = string("vl", replace(string(uuid4()), "-" => ""))
+    print(io, "<div id='$divid' style=\"width:100%;height:100%;\"></div>")
+    print(io, "<script type='text/javascript'>requirejs.config({paths:{'vg-embed': 'https://cdn.jsdelivr.net/npm/vega-embed@6?noext','vega-lib': 'https://cdn.jsdelivr.net/npm/vega-lib?noext','vega-lite': 'https://cdn.jsdelivr.net/npm/vega-lite@4?noext','vega': 'https://cdn.jsdelivr.net/npm/vega@5?noext'}}); require(['vg-embed'],function(vegaEmbed){vegaEmbed('#$divid',")    
+    VegaLite.our_json_print(io, v)
+    print(io, ",{mode:'vega-lite'}).catch(console.warn);})</script>")
+end
+# End snippet
 
 DocMeta.setdocmeta!(IteratedProcessSimulations, :DocTestSetup, :(using IteratedProcessSimulations); recursive=true)
 
